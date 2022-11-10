2126d3cbc15564f6a4f619102a9e8a83fec7b3ad
Oussama-El-Amrani <96589855+Oussama-El-Amrani@users.noreply.github.com>
2022-11-10 21:07:43 +0100

    Implementation du l'interface IDroite

--- formesGeometrique/Droite.cs
+++ formesGeometrique/Droite.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace formesGeometrique
 {
-    public class Droite
+    public class Droite : IDroite
     {
-       
+        public Point pointA { get; set; }
+        public Point pointB { get; set; }
+
+        public Droite(Point pointA, Point pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+
+        public bool Egalite(Droite droite)
+        {
+            return (pointA.Egalite(droite.pointA) && pointB.Egalite(droite.pointB));
+        }
+
+        public bool Parallelisme(Droite droite)
+        {
+            return (droite.pointA == pointA.Translation() && droite.pointB == pointB.Translation());
+            }
+
+        public bool Intersection(Droite droite)
+        {
+            double coefficientA = ((pointB.Y - pointA.Y) / (pointB.X - pointA.Y));
+            double coefficientB = ((droite.pointB.Y - droite.pointA.Y) / (droite.pointB.X - droite.pointA.X));
+
+            return coefficientA != coefficientB;
+        }
     }
 }
